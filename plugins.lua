8e6a4c65cce2a0fd8ecccf864e36fcdc753de434
Animesh Risal <animeshrisal99@gmail.com>
2024-06-24 21:20:14 +0200

    NVChad configuration

---
+++ plugins.lua
@@ -0,0 +1,63 @@
+local plugins = {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "nvimtools/none-ls-extras.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "eslint-lsp",
+        "prettierd",
+        "docker-compose-language-server",
+        "dockerfile-language-server",
+        "typescript-language-server",
+        "rust-analyzer",
+      }
+    }
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      local opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "rust",
+        "c"
+      }
+      return opts
+    end
+  }
+}
+return plugins
